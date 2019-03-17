65fbba7fddc452f1cf8ea089abfa3ca92ac33091
IsabelaOliveira272018 <isa.oliver20@gmail.com>
2019-03-17 17:12:15 -0300

    Iniciando o projeto de sistema de recomendações de filmes em python

---
+++ recomendacao.py
@@ -0,0 +1,10 @@
+#variavel avaliacoes recebendo valores
+avaliacoes = {'Ana': 
+		{'Freddy x Jason': 2.5, 
+		 'O Ultimato Bourne': 3.5,
+		 'Star Trek': 3.0, 
+		 'Exterminador do Futuro': 3.5, 
+		 'Norbit': 2.5, 
+		 'Star Wars': 3.0},
+	 
+	  }
